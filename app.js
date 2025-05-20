63c134314ba449a7f60ef31b2e03ff56bb920423
nijampurkarpooja <poojanijampurkar.1098@gmail.com>
2025-05-20 18:58:54 +0530

    setup upstash and workflow reminders

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import { PORT } from "./config/env.config.js";
 import connectToDB from "./database/mongodb.js";
 
 import errorMiddleware from "./middlewares/error.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/users", useRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,10 +1,23 @@
+import { SERVER_URL } from "../config/env.config.js";
+import { workflowClient } from "../config/upstash.config.js";
 import Subscription from "../models/subscription.model.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({ ...req.body, user: req.user._id });
 
-    res.status(201).json({ success: true, data: subscription });
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      retries: 0,
+    });
+
+    res.status(201).json({ success: true, data: { subscription, workflowRunId } });
   } catch (error) {
     next(error);
   }
